4c1eb21193c05c86e853bb21372d8b95cb07fe85
Ivan Angelkoski <ivanangelkoski95@gmail.com>
2023-06-05 22:29:20 +0200

    feat: nuxt app

---
+++ app.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="grid place-items-center text-center min-h-screen">
+    <div>
+      <h1>Nuxt + TailwindCss + Pinia + VueUse</h1>
+      <h2>Starter</h2>
+    </div>
+  </div>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,12 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  modules: ["@nuxtjs/tailwindcss", "@vueuse/nuxt", "@pinia/nuxt"],
+  pinia: {
+    autoImports: [
+      "defineStore", // import { defineStore } from 'pinia'
+    ],
+  },
+  imports: {
+    dirs: ["stores"],
+  },
+});
